0c601e9d597e3f37ef9e6ac7e476ab48236c8211
SergeM <sbmatyunin@gmail.com>
2018-01-20 11:25:13 +0100

    fixed fight2,

--- misc/fight2.sh
+++ misc/fight2.sh
@@ -1,5 +1,7 @@
 script_dir=$(cd `dirname $0` && pwd)
 
 echo $script_dir
+robot_path=$script_dir/../robots/
+echo "Robot path=$robot_path"
 
-java -Xmx1024M -cp ./libs/robocode.jar robocode.Robocode -battle $script_dir/battles.battle -results $script_dir/results.txt
+java -Xmx1024M -cp ./libs/robocode.jar -DROBOTPATH=$robot_path robocode.Robocode -battle $script_dir/battles2.battle -results $script_dir/results.txt
